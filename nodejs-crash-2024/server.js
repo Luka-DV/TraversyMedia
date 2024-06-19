c98bcb489919c3c4955547ca40d278e7d6950e37
Luka-DV <luka.guva@gmail.com>
2024-06-19 20:54:53 +0200

    edit simple server code and add nodemon

--- nodejs-crash-2024/server.js
+++ nodejs-crash-2024/server.js
@@ -3,10 +3,19 @@ const PORT = 8000;
 
 const server = http.createServer( (request, response) => {
     // response.write("Hello World!");
-    response.end("Hello Europe!"); //ends the stream, is handled by default in frameworks like express
+
+    // response.setHeader("Content-Type", "text/html");
+    // response.statusCode = 404;
+    // can be combined into:
+
+    response.writeHead(500, {"Content-Type": "application/json"});
+
+    //response.end("<h1>Hello Europe!</h1>"); //ends the stream, is handled by default in frameworks like express
+    response.end(JSON.stringify({message: "Server ErroR"})) //response is sending some json
 });
 
 //we have to listen for a port:
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
